dbd54879f66400da3eb6244a7c656d66bac4be6a
SecretAgent <75268452+SecretAgent-YT@users.noreply.github.com>
2022-02-02 22:03:46 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,89 @@
+currentGuess = ""
+guessIndex = 0;
+gameFinished = false
+
+words = [
+    'words',
+    'judge',
+    'throw',
+    'files',
+    'index',
+    'lines',
+    'enter',
+    'guess',
+    'games',
+    'finds'
+]
+
+word = words[Math.floor(Math.random()*words.length)]
+
+document.addEventListener("keypress", (event) => {
+    if (gameFinished) return
+    if (event.key == "Enter") {
+        submitGuess()
+    } else if (isLetter(event.key)) {
+        addLetter(event.key.toUpperCase())
+    }
+}, false)
+
+document.addEventListener("keydown", (event) => {
+    if (gameFinished) return
+    if (event.key == "Backspace") removeLetter()
+}, false)
+
+function addLetter(letter) {
+    if (currentGuess.length >= 5) return
+    currentGuess += letter;
+    updateCurrentGuess()
+}
+
+function removeLetter() {
+    currentGuess = currentGuess.slice(0, -1)
+    updateCurrentGuess()
+}
+
+function updateCurrentGuess() {
+    if (currentGuess == "") {
+        document.getElementById("currentGuess").innerHTML = "...";
+        return
+    }
+    document.getElementById("currentGuess").innerHTML = currentGuess;
+}
+
+function submitGuess() {
+    if (currentGuess.length < 5 || guessIndex == 6) return
+    guessIndex++;
+    for (var i = 0; i < currentGuess.length; i++) {
+        char = currentGuess.charAt(i);
+        if (word.charAt(i).toUpperCase() == char) {
+            document.getElementById("guess" + guessIndex).innerHTML += "<span class='correct'>" + char + "</span>";
+        } else if (word.includes(char.toLowerCase())) {
+            document.getElementById("guess" + guessIndex).innerHTML += "<span class='contains'>" + char + "</span>";
+        } else {
+            document.getElementById("guess" + guessIndex).innerHTML += "<span class='wrong'>" + char + "</span>";
+        }
+    }
+    if (currentGuess == word.toUpperCase()) {
+        endGame(true)
+    } else if (guessIndex == 6) {
+        endGame(false)
+    }
+    currentGuess = "";
+    updateCurrentGuess()
+}
+
+function endGame(won) {
+    gameFinished = true
+    document.getElementById("popup").style.display = "block";
+    if (won) {
+        document.getElementById("result").innerHTML = "Result: Victory"
+    } else {
+        document.getElementById("result").innerHTML = "Result: Defeat"
+    }
+    document.getElementById("word").innerHTML = "Word: " + word.toUpperCase()
+}
+
+function isLetter(char) {
+    return (/[a-zA-Z]/).test(char)
+}
+
